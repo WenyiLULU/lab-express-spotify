c9a57a676b350ca3c057f6b238a947e2834e7104
WenyiLULU <luwenyi1991@gmail.com>
2022-05-27 23:40:05 +0200

    step 1 done

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
 const SpotifyWebApi = require('spotify-web-api-node');
+const expressLayouts = require('express-ejs-layouts')
 
 // require spotify-web-api-node package here:
 
 const app = express();
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.use(expressLayouts)
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
@@ -24,6 +25,8 @@ const spotifyApi = new SpotifyWebApi({
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
-
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
